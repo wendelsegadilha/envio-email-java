b1bf96202833342308daaaed20859f9eb81196ab
Wendel Segadilha <wendelsegadilha99@gmail.com>
2021-01-25 13:46:47 -0300

    Estabelecendo conexão com servdor SMTP do Gmail

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,22 +1,39 @@
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 import org.junit.Test;
 
 public class AppTest {
-	
+
 	@Test
-	public void testEmail(){
-	
-		//configuração do servidor de email
-		Properties properties = new Properties();
-		properties.put("mail.smtp.auth", "true");//autorização
-		properties.put("mail.smtp.starttls", "true");//atenticação
-		properties.put("mail.smtp.host", "smtp.gmail.com");//servidor
-		properties.put("mail.smtp.port", "465");//porta
-		properties.put("mail.smtp.socketFactory.port", "465");//porta utilizada pelo socket
-		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");//classe socket para conexão SMTP
-		
-		
+	public void testEmail() {
+
+		try {
+
+			// configuração do servidor de email
+			Properties properties = new Properties();
+			properties.put("mail.smtp.auth", "true");// autorização
+			properties.put("mail.smtp.starttls", "true");// atenticação
+			properties.put("mail.smtp.host", "smtp.gmail.com");// servidor
+			properties.put("mail.smtp.port", "465");// porta
+			properties.put("mail.smtp.socketFactory.port", "465");// porta utilizada pelo socket
+			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");// classe socket para
+																								// conexão SMTP
+
+			// conexão com servidor Gmail
+			Session session = Session.getInstance(properties, new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("cursojavawebavancado2021@gmail.com", "brasil@2020");
+				}
+			});
+			System.out.println(session);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
